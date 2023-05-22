e11d3f435ab1add0600c31af349e439b32910d55
Armand Fatalot <armand.fatalot@gmail.com>
2023-05-22 21:46:11 +0200

    Initial commit

---
+++ frontend/src/components/home/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function ButtonAppBar() {
+
+  return (
+    <div >
+      <h1 className="text-3xl font-bold underline text-center ">Header </h1>
+    </div>
+  );
+}
---
+++ frontend/src/components/home/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './header'
+
+
+export default function index() {
+    return (
+        <div>
+         <Header/>
+        
+        
+
+        </div>
+    )
+}
